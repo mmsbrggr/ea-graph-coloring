2cb447d6625a5006adca4cd0f8afd4afa0f78da3
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2018-05-31 18:08:45 +0200

    Implemented mutation

--- src/mutate.py
+++ src/mutate.py
@@ -1,3 +1,12 @@
+import random
+
+mutation_prob = 0.1
+
+
 def mutate(individual):
-    # TODO return mutated individual
-    pass
+    if random.random() < mutation_prob:
+        index1 = random.randint(0, individual.coloring - 1)
+        index2 = random.randint(0, individual.coloring - 1)
+        tmp = individual.coloring[index1]
+        individual.coloring[index1] = individual.coloring[index2]
+        individual.coloring[index2] = tmp
