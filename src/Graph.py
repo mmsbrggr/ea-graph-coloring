0f195802d34b712fc29f6775fdf5a6a2365c6abe
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2018-05-31 16:43:35 +0200

    Implemented graph parsing

---
+++ src/Graph.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Graph:
+
+    def __init__(self, number_vertices):
+        self.adj_matrix = np.zeros((number_vertices, number_vertices))
+
+    def add_edge(self, vertex1, vertex2):
+        self.adj_matrix[vertex1][vertex2] = 1
+        self.adj_matrix[vertex2][vertex1] = 1
---
+++ src/Parser.py
@@ -0,0 +1,21 @@
+from Graph import Graph
+
+preamble_symbol = "p"
+edge_symbol = "e"
+
+
+class Parser:
+
+    @staticmethod
+    def get_graph_from_file(file_name):
+        graph = None
+        with open(file_name) as file:
+            lines = file.readlines()
+            for line in lines:
+                words = line.split()
+                if words[0] is preamble_symbol:
+                    graph = Graph(int(words[2]))
+                elif words[0] is edge_symbol:
+                    graph.add_edge(int(words[1]) - 1, int(words[2]) - 1)
+
+        return graph
